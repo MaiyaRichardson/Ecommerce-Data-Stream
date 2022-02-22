5455b5b627828865509e6d8e1ea5519507b046a4
QveenMai <scottmaiya@icloud.com>
2022-02-22 15:59:00 -0500

    Adding scala file

---
+++ src/main/scala/Ecommerce.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.SparkConf
+
+object EcommerceProj {
+    def main(args:Array[String]): Unit = {
+        val spark = SparkSession
+            .builder
+            .appName("KafkaSparkIntegration")
+            .master("local")
+            .getOrCreate()
+
+        spark.sparkContext.setLogLevel("WARN")
+
+        import spark.implicits._
+
+        val rdd = spark.sparkContext.parallelize(Array(("USA", "Boston"), ("India", "Mumbai"), ("trisha", 28), ("bob", 52), ("fred", 23)))
+        val df = rdd.toDF("Customer_Name", "age")
+
+        df.selectExpr("CAST(Customer_Name AS String) AS key", "CAST(Customer_ID AS String) AS value")
+            .write
+            .format("kafka")
+            .option("topic", "newtopic")
+            .option("kafka.bootstrap.servers","localhost:9092")
+            .option("checkpointLocation", "/home/gabrielklein/week11/KafkaSparkIntegration")
+            .save()
+
+        spark.close()
+
+        def randomStringGen(length: Int) = scala.util.Random.alphanumeric.take(length).mkString
+        val df = sparkContext.parallelize(Seq.fill(4000){(randomStringGen(4), randomStringGen(4), randomStringGen(6))}, 10).toDF("Order_ID", "Customer_Name", "Customer_ID")})
+        df.write.csv("s3://my-bucket/dummy-data/")
+    }
+
+
+    
+}
