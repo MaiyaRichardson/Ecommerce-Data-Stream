5c4748badfdcfa3a0725e1a5b84069fed8c69122
Alvin Lee <a3171998@gmail.com>
2022-03-02 18:58:43 -0800

    changes to .scala

---
+++ src/main/scala/Ecommerce.scala
@@ -0,0 +1,92 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.SparkConf
+import scala.util.Random.nextInt
+import org.apache.hadoop.yarn.webapp.hamlet.HamletSpec.P
+import java.util.Properties
+import java.util.UUID.randomUUID
+
+object EcommerceProj {
+    def main(args:Array[String]): Unit = {
+        /*val spark = SparkSession
+            .builder
+            .appName("KafkaSparkIntegration")
+            .master("local")
+            .getOrCreate()
+
+        spark.sparkContext.setLogLevel("WARN")
+
+        import spark.implicits._
+
+        val rdd = spark.sparkContext.parallelize(Array(("USA", "Boston"), ("India", "Mumbai"), ("trisha", 28), ("bob", 52), ("fred", 23)))
+        val df = rdd.toDF("Customer_Name", "age")
+
+        df.selectExpr("CAST(Customer_Name AS String) AS key", "CAST(Customer_ID AS String) AS value")
+            .write
+            .format("kafka")
+            .option("topic", "newtopic")
+            .option("kafka.bootstrap.servers","localhost:9092")
+            .option("checkpointLocation", "/home/gabrielklein/week11/KafkaSparkIntegration")
+            .save()
+
+        spark.close()
+
+        def randomStringGen(length: Int) = scala.util.Random.alphanumeric.take(length).mkString
+        var name = "dsd"
+        var Id = "sdsd"
+        //var website
+        //val df = sparkContext.parallelize(Seq.fill(4000){(randomStringGen(4), randomStringGen(4), randomStringGen(6))}, 10).toDF("Order_ID", "Customer_Name", "Customer_ID")})
+        df.write.csv("s3://my-bucket/dummy-data/")
+        */
+        randomGenerator()
+    }
+
+    def randomGenerator(): Unit = {
+        
+            val firstNames = List("Steve", "Tony", "Peter", "Miles", "Cameron", "Kyle", "Brandon", "Summer", "Sunshine", "Autumn", "Sharyar", "Keisha", "Hardik", "Daulton", "Abubacarr", "Hardik", "Giancarlos", "Alvin", "Mai")
+            val nouns = List("Parker", "Stark", "Rodgers", "moon", "rain","wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf","sequoia", "cedar", "wrath", "blessing", "spirit", "nova", "storm", "burst","giant", "elemental", "throne", "game", "weed", "stone", "apogee", "bang")
+        
+            var res2 = 0
+        
+        def getRandElt[A](xs: List[A]): A = xs.apply(nextInt(xs.size))
+    
+        def getRandNumber(ra: Range): String = {
+            (ra.head + nextInt(ra.end - ra.head)).toString
+        }
+            
+        def haiku: String = {
+            val xs = getRandNumber(1000 to 9999) :: List(nouns, firstNames).map(getRandElt)
+            xs.reverse.mkString(",")
+
+            
+        }
+        println(haiku)
+        
+        def order_id: BigInt = {
+        // val res2 = randomUUID().toString
+            res2 += 1
+            return res2
+        }
+
+        def customer_id: String = {
+            val res2 = randomUUID().toString
+            return res2
+        }
+
+
+        while(true){
+// order id
+        println(order_id + ", " + customer_id)
+
+// customer id
+// println(customer_id)
+        Thread.sleep(2000)
+}
+
+
+        
+    }
+    
+
+    
+}
