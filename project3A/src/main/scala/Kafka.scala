2f75dfa67e74670b349a63310f292088d467b077
QveenMai <scottmaiya@icloud.com>
2022-03-09 21:11:29 -0500

    added 'project3A' directory

---
+++ project3A/src/main/scala/Kafka.scala
@@ -0,0 +1,77 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka
+import org.apache.spark.streaming.StreamingContext._
+import scala.collection.mutable.ArrayBuffer
+import java.io.{BufferedWriter, OutputStreamWriter}
+import scala.collection.JavaConversions._
+import scala.util.Random
+import java.io.{File, FileWriter}
+import au.com.bytecode.opencsv.CSVWriter
+import scala.collection.mutable.ListBuffer
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SparkSession
+
+object Kafka {
+    var a=true
+    def main(args: Array[String]): Unit = {
+        val spark=SparkSession
+        .builder
+        .appName("Consumer")
+        .config("spark.master", "local")
+        .getOrCreate()
+    
+   // while (a==true)
+   //Thread.sleep(20000)
+    spark.sparkContext.setLogLevel("WARN")
+
+    
+        var lines=spark
+        .readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "mytest")
+        .option("failOnDataLoss", "false")
+        .load()
+        var r=lines.selectExpr("Cast(key AS STRING)", "CAST(value AS STRING)")
+    
+        
+        
+        r.writeStream
+        .format("csv")
+        .option("path", "/mnt/c/Users/user/Desktop/project3A")
+        .option("failOnDataLoss", "false")
+        .option("checkpointLocation",  "/mnt/c/Users/user/Desktop/project3A/src")
+        .start()
+        //.awaitTermination(5000)
+        //.stop()
+
+        var r2=r
+        /*  spark.sparkContext.setLogLevel("WARN")
+            lines=spark
+            .readStream
+            .format("kafka")
+            .option("kafka.bootstrap.servers", "localhost:9092")
+            .option("subscribe", "mytest")
+            .load() */
+        for (i<-0 to 5){
+            
+            r=lines.selectExpr("Cast(key AS STRING)", "CAST(value AS STRING)")
+            r2.union(r)
+            Thread.sleep(1000)
+        }
+    
+        r2.writeStream
+        .format("csv")
+        .option("path", "/mnt/c/Users/user/Desktop/project3A")
+        .option("failOnDataLoss", "false")
+        //.option("checkPointLocation",  "/c/Users/leifr/Desktop/Kafka")
+        .option("checkpointLocation",  "/mnt/c/Users/user/Desktop/project3A")
+        .start()
+        .awaitTermination(5000)
+    }
+
+}
---
+++ project3A/src/main/scala/queryData.scala
@@ -0,0 +1,164 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql.SQLContext
+import java.sql.DriverManager
+import java.sql.Connection
+import java.util.Scanner
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions.{min, max}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.execution.aggregate.HashAggregateExec
+import org.apache.spark.sql.execution.joins.BroadcastHashJoinExec
+import org.apache.spark.sql.expressions.scalalang.typed
+import org.apache.spark.sql.functions.{avg, broadcast, col, max}
+import org.apache.spark.sql.types
+import org.apache.hadoop.fs.HardLink
+import org.apache.spark.sql.functions.{to_date, to_timestamp}
+import org.apache.spark.sql.types.{IntegerType,StringType,StructType,StructField}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.IntegerType
+
+
+
+object queryData {
+    val conf = new SparkConf().setMaster("local").setAppName("Covid")
+    private val sc = new SparkContext(conf)
+    private val hiveCtx = new HiveContext(sc)
+
+    def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+            .builder
+            .appName("ProjMain")
+            .master("local")
+            .getOrCreate()
+        spark.sparkContext.setLogLevel("WARN")
+        import spark.implicits._
+
+    //This block of code is all necessary for spark/hive/hadoop
+    
+    // System.setSecurityManager(null)
+    // System.setProperty("hadoop.home.dir", "C:\\hadoop\\") // change if winutils.exe is in a different bin folder
+    // // val conf = new SparkConf()
+    // //     .setMaster("local") 
+    // //     .setAppName("Covid")    // Change to whatever app name you want
+    // //val sc = new SparkContext(conf)
+    // sc.setLogLevel("ERROR")
+    // //val hiveCtx = new HiveContext(sc)
+    // import hiveCtx.implicits._
+    // val spark = SparkSession
+    // .builder
+    // .appName("Covid")
+    // .config("spark.master", "local")
+    // .config("spark.eventLog.enabled", "false")
+    // .getOrCreate()
+
+    
+    spar(spark)
+    // spark.stop()
+    // insertKafkaData(hiveCtx)
+    // maxPurchase()
+    // minPurchase()
+    // orderCount()
+        
+    }
+
+    //def insertKafkaData(hiveCtx:HiveContext): Unit = {
+
+        // val custom_schema = StructType(Array(
+        //     StructField("order_id", StringType, true),
+        //     StructField("customer_id", StringType, true),
+        //     StructField("customer_name", StringType, true),
+        //     StructField("product_id", StringType, true),
+        //     StructField("product_name", StringType, true),
+        //     StructField("product_category", StringType, true),
+        //     StructField("payment_type", StringType, true),
+        //     StructField("qty", StringType, true),
+        //     StructField("price", StringType, true),
+        //     StructField("datetime", StringType, true),
+        //     StructField("country", StringType, true),
+        //     StructField("city", StringType, true),
+        //     StructField("ecommerce_website_name", StringType, true),
+        //     StructField("payment_txn_id", StringType, true),
+        //     StructField("payment_txn_success", StringType, true),
+        //     StructField("failure_reason", StringType, true)
+        // ))
+    
+    def spar(spark:SparkSession): Unit={
+        val df = spark.read.csv("input/team2_data.csv").toDF("order_id","customer_id","customer_name","product_id","product_name","product_category","payment_type","qty","price","datetime","country","city","ecommerce_website_name","payment_txn_id","payment_txn_success","failure_reason")
+
+        // //val f= df.where(col("payment_txn_succcess" = "Y"))
+        // val f = df.where(df("payment_txn_success") === "N")
+        // f.show()
+        // f.write.csv("input/Hardik's")
+        // //var co =f.count()
+        // //println("total: "+co)
+
+
+        var newdf = df.withColumn("qty",col("qty").cast(IntegerType))
+        newdf.createOrReplaceTempView("df2")
+        df.createOrReplaceTempView("df1")
+        
+
+        var query = "Select * From df1"
+
+        val f= spark.sql(query)
+        f.show(100,100)
+
+        var maxPurchase = "Select country,max(qty) from df2 group by country"
+        //var maxPurchase = "select qty,product_name from df2 where country = \"Germany\" "
+        val g = spark.sql(maxPurchase)
+        g.show(10)
+        g.repartition(1).write.format("com.databricks.spark.csv").mode("overwrite").csv("input/groupedCountry")
+        //g.write.csv("input/germanycountryPurchase")
+
+    }
+
+
+
+    //     val output = hiveCtx.read
+    //         .format("csv")
+    //         .option("inferschema", "true")
+    //         .option("header", "true")
+    //         .option("delimiter", ",")
+    //         .load("input/team2_data.csv")
+
+    //         //output.printSchema()
+    //     output.limit(15).show() // Prints out the first 15 lines of the dataframe
+
+    //     // output.registerTempTable("data2") // This will create a temporary table from your dataframe reader that can be used for queries. 
+
+    //     output.createOrReplaceTempView("temp_data4")
+    //     //hiveCtx.sql("DROP TABLE IF EXISTS covid1")    
+    //     hiveCtx.sql("CREATE TABLE IF NOT EXISTS Kafka (order_id STRING,customer_id STRING,customer_name STRING,product_id STRING,product_name STRING,product_category STRING,payment_type STRING,qty STRING,price STRING,datetime STRING,country STRING,city STRING,ecommerce_website_name STRING,payment_txn_id STRING,payment_txn_success STRING,failure_reason STRING) ROW FORMAT DELIMITED BY ',' FIELDS TERMINATED BY ','")
+    //     hiveCtx.sql("INSERT INTO Kafka SELECT * FROM temp_data4")
+    //     val summary = hiveCtx.sql("SELECT * FROM Kafka LIMIT 10")
+        
+    //     summary.show()
+    //     summary.write.mode("overwrite").csv("results/view10")       
+    // }
+    // def maxPurchase(): Unit = {
+    //     val result = hiveCtx.sql("Select MAX(price), order_id, product_name, qty, datetime, customer_name from Kafka limit 1")
+    //     result.show()
+    //     result.write.csv("input/maxPurchase")
+    // }
+    // def minPurchase(): Unit = {
+    //     val result =
+    //     hiveCtx.sql("Select MIN(price), order_id, product_name, qty, datetime, customer_name from Kafka limit 1")
+    //     result.show()
+    //     result.write.csv("input/minPurchase")
+    // }
+
+    // def orderCount(): Unit = {
+    //     val result =
+    //     hiveCtx.sql("Select Count(payment_txn_success) from Kafka")
+    //     result.show()
+    //     result.write.csv("input/orderCount")
+    // }
+    //}
+}
